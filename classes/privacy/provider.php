30550e9d5806f0ab617ebe226c7bd6cdc291145d
GGeorggg <georg-github.com@glas.eu.org>
2023-08-17 14:22:08 +0200

    Unexpected MOODLE_INTERNAL check. No side effects or multiple artifacts detected.
    https://github.com/center-for-learning-management/moodle-availability_gps/issues/3

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -24,8 +24,6 @@
 namespace availability_gps\privacy;
 use core_privacy\local\metadata\collection;
 
-defined('MOODLE_INTERNAL') || die;
-
 class provider implements \core_privacy\local\metadata\provider {
     public static function get_metadata(collection $collection) : collection {
         // Here you will add more items into the collection.
